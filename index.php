d5e668ca1d6692f4fd4fa3d52aa96df61a176cd7
Alisa202023 <60154547+Alisa202023@users.noreply.github.com>
2021-07-06 15:13:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 include_once("myform.html");
-include_once("css/intlTelInput.css");
-include_once("css/style.css");
-
-include_once("js/jquery.min.js");
-include_once("js/intlTelInput.js");
-include_once("js/utils.js");
-include_once("js/main.js");
 ?>
