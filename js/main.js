153bad2faad6870ad4293bef28129b169142dcc2
Alisa202023 <60154547+Alisa202023@users.noreply.github.com>
2021-07-09 15:11:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var n = document.getElementById("00N5g00000HuOR5").options.selectedIndex;
 var val = document.getElementById("00N5g00000HuOR5").options[n].value;
 var btn = document.getElementById("btn") ;
 
-if (val==""){btn.disabled=true;}
+
 
 /*---------------------RECAPTCHA-----------------------------*/
 function scaleCaptcha(elementWidth) {
@@ -35,11 +35,12 @@ jQuery(function() {
 //for trottle
 (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
 
-function recaptcha_callback() {
-                jQuery( "#btn" ).removeAttr('disabled');
-	
-		
-};    
+document.getElementById('sendForm').onsubmit = function () {
+  if (!grecaptcha.getResponse()) {
+       alert('You not checked the I am not a robot!');
+       return false; 
+  }
+}   
 /*--------------------------PHONE--------------------------------*/
 
 var input = document.querySelector("#phone"),
